0ed709e6fb2ab5262d9856a7dd780ce42d1754e7
rainingYao <18662035421@163.com>
2019-01-20 16:22:10 +0800

    添加一题：将指定数与自然数列相乘，计算找到所有十进制数字时的深度。

---
+++ Kata/Integer depth/IntegerDepth.cs
@@ -0,0 +1,96 @@
+﻿namespace IntegerDepth
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(12, Kata.ComputeDepth(8));
+        }
+
+        [Test]
+        public void EdgeCase1()
+        {
+            Assert.AreEqual(10, Kata.ComputeDepth(1));
+        }
+
+        [Test]
+        public void EdgeCase2()
+        {
+            Assert.AreEqual(10, Kata.ComputeDepth(7));
+        }
+
+        [Test]
+        public void EdgeCase3()
+        {
+            Assert.AreEqual(12, Kata.ComputeDepth(8));
+        }
+
+        [Test]
+        public void EdgeCase4()
+        {
+            Assert.AreEqual(8, Kata.ComputeDepth(13));
+        }
+
+        [Test]
+        public void EdgeCase5()
+        {
+            Assert.AreEqual(36, Kata.ComputeDepth(25));
+        }
+
+        [Test]
+        public void EdgeCase6()
+        {
+            Assert.AreEqual(9, Kata.ComputeDepth(42));
+        }
+
+        private static int ComputeDepthPrivately(int n)
+        {
+            var f = new HashSet<char>();
+            var d = 0;
+
+            while (f.Count < 10)
+            {
+                var value = (n * ++d).ToString();
+                foreach (char c in value)
+                    if (!f.Contains(c))
+                        f.Add(c);
+            }
+
+            return d;
+        }
+
+        [Test]
+        public void RandomTests()
+        {
+            var random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var e = random.Next(1, 501);
+                var expected = ComputeDepthPrivately(e);
+                var actual = Kata.ComputeDepth(e);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+
+    public class Kata
+    {
+
+        public static int ComputeDepth(int n)
+        {
+            string all = "0123456789";
+            int i = 1;
+            for (; all != ""; i++)
+                foreach (char c in (n * i).ToString())
+                    all = all.Replace(c.ToString(), "");
+            return i - 1;
+        }
+
+    }
+}
--- Kata/README.md
+++ Kata/README.md
@@ -2,9 +2,12 @@
 |---|---|
 |<5 kyu> [Primes in numbers]|[/Primes in numbers]|
 |<6 kyu> [Duplicate Encoder]|[/Duplicate Encoder]|
+|<6 kyu> [Integer depth]|[/Integer depth]|
 
 
 [Primes in numbers]: https://www.codewars.com/kata/54d512e62a5e54c96200019e
 [/Primes in numbers]: Primes%20in%20numbers
 [Duplicate Encoder]: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
 [/Duplicate Encoder]: Duplicate%20Encoder
+[Integer depth]: https://www.codewars.com/kata/59b401e24f98a813f9000026
+[/Integer depth]: Integer%20depth
