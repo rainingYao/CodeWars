a27b2d8484c288cf204608dda343a8c6d0b79f4e
rainingYao <18662035421@163.com>
2019-09-13 21:34:16 +0800

    Add a question I can't do

---
+++ Kata/a question set/Two Letters in One Line Task.js
@@ -0,0 +1,6 @@
+// <5 kyu> One Line Task: Two Letters
+// https://www.codewars.com/kata/one-line-task-two-letters
+
+toASCIINumber=(a,b)=>(a.charCodeAt()-'A'.charCodeAt()).toString()+(b.charCodeAt()-'A'.charCodeAt()).toString()
+//How to code in one line and less than 54 characters?
+
